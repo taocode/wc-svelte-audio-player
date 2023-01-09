753b0950bab4fadbb5a56a39df448a9c3d0e28a9
taocode <mark@taocode.com>
2023-01-09 10:38:08 -0500

    feat(stories): start to add examples to stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,10 +1,14 @@
-import "../src/index.svelte";
-import { html } from "lit-html";
+import "../src/index.svelte"
+import "../src/Audio.svelte"
+import { html } from "lit-html"
 
 export default {
   parameters: {
     layout: "centered",
   },
+  title: 'Single & Multiple Track Examples',
 };
 
-export const story1 = () => html` <my-counter></my-counter> `;
+export const singleTrack = () => html` <taocode-audio-player><taocode-audio src="https://assets.mixkit.co/sfx/preview/mixkit-mystwrious-bass-pulse-2298.mp3" title="Mixkit mystwrious bass pulse"></taocode-audio-player> `;
+
+export const multipleTracks = () => html` <taocode-audio-player><taocode-audio src="https://assets.mixkit.co/sfx/preview/mixkit-mystwrious-bass-pulse-2298.mp3" title="Mixkit mystwrious bass pulse"></taocode-audio-player> `;
